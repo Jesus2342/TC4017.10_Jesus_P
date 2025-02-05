916665156b172bf99a80f80be27e63314ae0b657
Jesus2342 <103007364+Jesus2342@users.noreply.github.com>
2025-02-05 05:59:48 -0700

    Adding Flake8 changes

--- A01794940_Actividad5.2/compute_sales.py
+++ A01794940_Actividad5.2/compute_sales.py
@@ -1,12 +1,12 @@
 """
-Instituto Tecnologico y de Estudios Superiores de Monterrey 
-Student: Jose de Jesus Pena Rodriguez 
+Instituto Tecnologico y de Estudios Superiores de Monterrey
+Student: Jose de Jesus Pena Rodriguez
 Student ID: A01794940
 Programming Exercise: Compute sales
-Description: The program shall compute the total cost for all sales 
+Description: The program shall compute the total cost for all sales
 
-Usage: 
-Type the name of the text file in INPUT_FILE then 
+Usage:
+Type the name of the text file in INPUT_FILE then
 run ---> python compute_sales.py TC1.ProductList.json TC1.Sales.json
 run ---> python compute_sales.py TC1.ProductList.json TC2.Sales.json
 run ---> python compute_sales.py TC1.ProductList.json TC3.Sales.json
@@ -17,8 +17,6 @@ import json
 import time
 
 start_time = time.time()
-
-
 
 PRODUCT_LIST_JSON = sys.argv[1]
 sales_list_json = sys.argv[2]
@@ -33,27 +31,27 @@ if len(sys.argv) != 3:
 
 
 system_output.append(f"Results from {TEST_CASE}")
-system_output.append("-----------------------------------------------------")
+system_output.append("-------------------------------------------")
 system_output.append("Files to be processed")
 system_output.append(f"Product list ----> {PRODUCT_LIST_JSON}")
 system_output.append(f"Sales list  -----> {sales_list_json}")
 
 
 try:
-    with open(PRODUCT_LIST_JSON, "r",encoding="utf-8") as file:
+    with open(PRODUCT_LIST_JSON, "r", encoding="utf-8") as file:
         PRODUCT_LIST_JSON = json.load(file)
-        system_output.append("-----------------------------------------------------")
+        system_output.append("-------------------------------------------")
         system_output.append("** Product list loaded")
 except FileNotFoundError:
-    error_msg_1= f"Error: Product list {PRODUCT_LIST_JSON} not found."
+    error_msg_1 = f"Error: Product list {PRODUCT_LIST_JSON} not found."
     print(error_msg_1)
     system_output.append(error_msg_1)
     sys.exit(1)
 try:
-    with open(sales_list_json, "r",encoding="utf-8") as file:
+    with open(sales_list_json, "r", encoding="utf-8") as file:
         sales_list_json = json.load(file)
         system_output.append("** Sales list loaded")
-        system_output.append("-----------------------------------------------------")
+        system_output.append("-------------------------------------------")
 except FileNotFoundError:
     error_msg_2 = f"Error: Sales list {sales_list_json} not found."
     print(error_msg_2)
@@ -76,16 +74,16 @@ for sale in sales_list_json:
         print(error_msg3)
         system_output.append(error_msg3)
 system_output.append(f"Total is {round(TOTAL_SALES,2)}")
-system_output.append("-----------------------------------------------------")
+system_output.append("-------------------------------------------")
 
 end_time = time.time()
 execution_time = round(end_time - start_time, 4)
 system_output.append(f"Execution Time: {execution_time} seconds")
-system_output.append("-----------------------------------------------------")
+system_output.append("-------------------------------------------")
 
 for line in system_output:
     print(line)
-with open(OUTPUT_FILE, "w",encoding="utf-8") as report_file:
+with open(OUTPUT_FILE, "w", encoding="utf-8") as report_file:
     for line in system_output:
         report_file.write(line + "\n")
-        
+    report_file.write("\n")
